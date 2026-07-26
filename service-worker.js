f37fe672cb1741809e860095607937d7ea3a9936
bmenschner <mail@bastianmenschner.de>
2026-07-26 15:58:34 +0200

    Make person relation markers clickable

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'sr6-app-v37';
-const RUNTIME_CACHE = 'sr6-runtime-v37';
+const APP_CACHE = 'sr6-app-v38';
+const RUNTIME_CACHE = 'sr6-runtime-v38';
 const APP_ENTRY = new URL('./index.html', self.registration.scope).href;
 const APP_SHELL = [
   './index.html',
